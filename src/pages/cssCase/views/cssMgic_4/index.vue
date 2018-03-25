54382f31a881b73f84f5daed991376c5e8f2ed6a
josephvong <m15914375662@163.com>
2018-03-25 15:03:32 +0800

    20180325

--- src/pages/cssCase/views/cssMgic_4/index.vue
+++ src/pages/cssCase/views/cssMgic_4/index.vue
@@ -29,10 +29,37 @@
             </pre>
           </div> 
         </div>
-      </div>
-        
+      </div> 
     </div>  
-
+    
+    <div class="page-body">
+      <div class="case">
+        <h3 class="case-name"> filter函数 </h3> 
+        <div class="sample-wrap case_b clearfix">
+          <div class="sample sample_1">blur</div>
+          <div class="sample sample_2">brightness</div>
+          <div class="sample sample_3">contrast</div>
+          <div class="sample sample_4">drop shadow</div>   
+        </div> 
+        <div class="code-wrap"> 
+          <!-- <div class="code"  v-highlight>
+            <p>html: </p> 
+            <pre>
+              <code class="html">
+                
+              </code>
+            </pre>
+          </div>-->
+          <div class="code"  v-highlight>  
+            <p>css:</p>
+            <pre>
+              <code> 
+              </code>
+            </pre>
+          </div> 
+        </div>
+      </div> 
+    </div> 
   </div>
 </template>
 
@@ -120,4 +147,43 @@ export default {
     &.sample_5
       box-shadow: 5px 0 5px -5px black,
                   -5px 0 5px -5px black;      
+.case_b
+  padding 10px
+  border 1px solid red
+  .sample
+    float left 
+    width 100px
+    height 100px
+    display flex
+    justify-content center
+    align-items center
+    margin 10px
+    line-height 22px
+    color white
+    font-size 18px
+    font-weight 700
+    text-align center
+    transition all 0.5s
+    background red
+  .sample_1
+    filter blur(0px) 
+    &:hover 
+      filter blur(10px)
+  .sample_2
+    filter brightness(10%) 
+    &:hover 
+      filter brightness(100%)
+  .sample_3
+    filter contrast(0%)
+    &:hover
+      filter contrast(90%)
+  .sample_4
+    border 1px solid black
+    background transparent
+    color black
+    filter drop-shadow(10px,10px,5px ,blue)
+    &:hover
+      background red
+      filter drop-shadow(40px,40px,5px ,blue)
+       
 </style> 
